4e558cf115eb52dccb6f31ad08c67a64ebd93bc4
Pep Villalonga Amorós <pep.villalonga@platoniq.net>
2026-09-25 15:38:22 +0200

    chore: import zod from zod package in actions (#506)

--- src/actions/highlights.ts
+++ src/actions/highlights.ts
@@ -1,5 +1,5 @@
-import { z } from "astro/zod";
 import { ActionError, defineAction } from "astro:actions";
+import { z } from "zod";
 
 import { highlightRepository } from "../repositories/highlights";
 
